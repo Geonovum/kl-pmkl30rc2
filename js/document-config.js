09006bf92b25f675e604975a5d7bb70a206a83da
Geonovum <>
2024-12-04 14:13:20 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/c9e02f7a331d0fed9acfce6a7b9f913e4653a530

---
+++ js/document-config.js
@@ -0,0 +1,46 @@
+var documentConfig =
+{
+  title: "Handreiking Visualisatie (PMKL) - versie 3.0rc2",
+  shortName: "pmkl30rc2",
+  pubDomain: "kl",
+  specStatus: "cv",
+  specType: "st",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/kl/pmkl30rc2/"
+  ],
+  publishDate: "2024-12-04",
+  publishVersion: "3.0.0",
+  thisVersion: [
+    "https://docs.geostandaarden.nl/kl/cv-st-pmkl30rc2-20241204/"
+  ],
+  previousPublishDate: "2024-09-30",
+  previousPublishVersion: "3.0.0",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/kl/cv-st-pmkl30rc2-20240930/"
+  ],
+  previousMaturity: "cv",
+  github: "https://github.com/geonovum/kl-pmkl30rc2/",
+  issueBase: "https://github.com/geonovum/kl-pmkl30rc2/issues/",
+  edDraftURI: "https://geonovum.github.io/kl-pmkl30rc2/",
+  canonicalURI: "https://docs.geostandaarden.nl/kl/pmkl30rc2/",
+  authors: [
+    {
+      name: "Edward Mac Gillavry",
+      company: "Webmapper",
+      companyURL: "https://webmapper.net/"
+    }
+  ],
+  editors: [
+    {
+      name: "Edward Mac Gillavry",
+      company: "Webmapper",
+      companyURL: "https://webmapper.net/"
+    },
+    {
+      name: "Paul Janssen",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ]
+}
